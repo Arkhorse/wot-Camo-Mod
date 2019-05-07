58e4616f76e2d955215144b647ea8a9551657184
Arkhorse <theillusivemman@gmail.com>
2019-05-07 03:29:19 -0300

    removed remaining garage settings bits

--- mod_camo_indicator.py
+++ mod_camo_indicator.py
@@ -3,10 +3,8 @@
 # connect the game engine libraries available in the game client built-in python
 import logging
 import BigWorld
-from mod_constants_camo import MOD, CONFIG_FILES
+import BattleReplay
 from debug_utils import LOG_CURRENT_EXCEPTION
-from gui import SystemMessages
-from Account import Account
 from Avatar import PlayerAvatar
 from skeletons.gui.app_loader import IAppLoader, GuiGlobalSpaceID
 from gui.app_loader.settings import APP_NAME_SPACE
@@ -17,11 +15,11 @@ from gui.battle_control.battle_constants import CROSSHAIR_VIEW_ID
 from BattleFeedbackCommon import BATTLE_EVENT_TYPE
 from gui.battle_control.controllers import feedback_events
 
+from mod_constants_camo import MOD, CONFIG_FILES
 # noinspection PyUnresolvedReferences
 # from gui.mods.mod_mods_gui import g_gui, inject
 
 _logger = logging.getLogger(MOD.NAME)
-
 def getLogLevel(name):
     logLevel = {
         'CRITICAL':     logging.CRITICAL,
@@ -32,22 +30,24 @@ def getLogLevel(name):
         'NOTSET':       logging.NOTSET
     }
     return logLevel.get(name, logging.INFO)
-_logger.info('Staring Camo Indicator')
-COLORS = ['#FE0E00', '#FE7903', '#F8F400', '#60FF00', '#02C9B3', '#D042F3']
-MENU = ['UI_color_blue', 'UI_color_brown', 'UI_color_chocolate', 'UI_color_cornflower_blue', 'UI_color_cream', 'UI_color_cyan', 'UI_color_emerald', 'UI_color_gold', 'UI_color_green', 'UI_color_green_yellow', 'UI_color_hot_pink', 'UI_color_lime',
-        'UI_color_orange', 'UI_color_pink', 'UI_color_purple', 'UI_color_red', 'UI_color_wg_blur', 'UI_color_wg_enemy', 'UI_color_wg_friend', 'UI_color_wg_squad', 'UI_color_yellow', 'UI_color_nice_red', 'UI_color_very_bad', 'UI_color_bad', 'UI_color_normal', 'UI_color_good', 'UI_color_very_good', 'UI_color_unique']
 
 GENERATOR = {
     BATTLE_EVENT_TYPE.VISIBILITY_EVENTS     : ['UI_message_VISIBILITY_EVENTS_text', 'messageColorVISIBILITY_EVENTS']
 }
 
 # Mod Start
-def modStart(self):
-    parent(self)
-    SystemMessages.pushMessage('Camo Indicator Loaded', type=SystemMessages.SM_TYPE.Warning)
-    Account.onBecomePlayer = parent
-parent = Account.onBecomePlayer
-Account.onBecomePlayer = modStart
+
+def onAppInitialized(self, event):
+        if event.ns != APP_NAME_SPACE.SF_BATTLE:
+            return
+        _logger.info('AppLifeCycleEvent.INITIALIZED: SF_BATTLE')
+        #self.initPanel()
+
+def onAppDestroyed(self, event):
+        if event.ns != APP_NAME_SPACE.SF_BATTLE:
+            return
+        _logger.info('AppLifeCycleEvent.DESTROYED: SF_BATTLE')
+        self.finiPanel()
 
 def format_recreate(self):
         self.format_str = {
@@ -55,10 +55,11 @@ def format_recreate(self):
         }
         
 def textGenerator(self, event):
-        text, color = GENERATOR[event]
-        return config.i18n[text].format(**self.format_str), COLOR[config.data[color]]
+        text
         
 def post_message(self, events):
+    try:
+        _logger.info('message started')
         g_sessionProvider = BigWorld.player().guiSessionProvider
         self.format_recreate()
         for data in events:
@@ -66,7 +67,8 @@ def post_message(self, events):
             eventID = feedbackEvent.getBattleEventType()
             if eventID in [BATTLE_EVENT_TYPE.VISIBILITY_EVENTS]:
                 vehicleID = feedbackEvent.getTargetID()
-                text, colour = self.textGenerator(eventID)
-                inject.message(text, color)
-                
+                text
+                inject.message(text)
+    except:
+        _logger.critical('Message Failed')
 print '[LOAD_MOD]: [Camo Indicator by The Illusion]'
--- mod_constants_camo.py
+++ mod_constants_camo.py
@@ -3,12 +3,12 @@ from skeletons.gui.app_loader import GuiGlobalSpaceID
 class MOD:
     ID = 'Camo Indicator'
     PACKAGE_ID = '${package_id}'
-    NAME = '${name}'
+    NAME = 'Camo Indicator'
     VERSION = '${version}'
 
-CONFIG_FILES = [
-    '${resource_dir}/config.json',
-    '${config_file}'
-]
+#CONFIG_FILES = [
+#    '${resource_dir}/config.json',
+#    '${config_file}'
+#]
 
 LOG_FILE = '${log_file}'
