7d5d63f9be3f7c440195bc19f4160be20aa411fb
Arkhorse <theillusivemman@gmail.com>
2019-05-09 00:51:55 -0300

    updated gitignore | upd camo core, and camo constants

--- mod_camo_indicator.py
+++ mod_camo_indicator.py
@@ -16,8 +16,7 @@ from BattleFeedbackCommon import BATTLE_EVENT_TYPE
 from gui.battle_control.controllers import feedback_events
 
 from mod_constants_camo import MOD, CONFIG_FILES
-# noinspection PyUnresolvedReferences
-# from gui.mods.mod_mods_gui import g_gui, inject
+
 
 _logger = logging.getLogger(MOD.NAME)
 def getLogLevel(name):
@@ -32,43 +31,79 @@ def getLogLevel(name):
     return logLevel.get(name, logging.INFO)
 
 GENERATOR = {
-    BATTLE_EVENT_TYPE.VISIBILITY_EVENTS     : ['UI_message_VISIBILITY_EVENTS_text', 'messageColorVISIBILITY_EVENTS']
+    BATTLE_EVENT_TYPE.VISIBILITY_EVENTS     : ['UI_message_VISIBILITY_EVENTS_text']
 }
+try:
+    # noinspection PyUnresolvedReferences
+    from gui.mods.mod_mods_gui import g_gui, inject
+except:
+    _logging.CRITICAL('Settings Failed')
+# Ingame Config
 
+class Config(object):
+    def __init__(self):
+        self.ids = 'camoIndicator'
+        self.version = 'v0.01 (2019-05-07)'
+        self.version_id = 001
+        self.author = 'by Illusion'
+        self.data = {
+            'version'                : self.version_id,
+            'enabled'                : True,
+            'camo'                   : True
+        }
+        self.i18n = {
+            'version'                                   : self.version_id,
+            'UI_description'                            : 'Camo Indicator',
+            'UI_camo_text'                              : 'Show Camo values in battle'
+        }
+        self.data, self.i18n = g_gui.register_data(self.ids, self.data, self.i18n, 'Illusion')
+        g_gui.register(self.ids, self.template, self.data, self.apply)
+        print '[LOAD_MOD]:  [%s %s, %s]' % (self.ids, self.version, self.author)
+        config = Config()
+        
+    def template(self):
+        return {
+            'modDisplayName' : self.i18n['UI_description'],
+            'settingsVersion': self.version_id,
+            'enabled'        : self.data['enabled'],
+            'column1'        : [{
+                'type'   : 'CheckBox',
+                'text'   : self.i18n['UI_description'],
+                'value'  : self.data['camo'],
+                'tooltip': self.i18n['UI_camo_text'],
+                'varName': 'camo'
+                }]        
 # Mod Start
 
-def onAppInitialized(self, event):
-        if event.ns != APP_NAME_SPACE.SF_BATTLE:
-            return
-        _logger.info('AppLifeCycleEvent.INITIALIZED: SF_BATTLE')
-        #self.initPanel()
-
-def onAppDestroyed(self, event):
-        if event.ns != APP_NAME_SPACE.SF_BATTLE:
-            return
-        _logger.info('AppLifeCycleEvent.DESTROYED: SF_BATTLE')
-        self.finiPanel()
-
-def format_recreate(self):
+def apply(self, settings):
+    self.data = g_gui.update_data(self.ids, settings, 'illusion')
+    g_gui.update(self.ids, self.template)
+        
+class Camo(object):
+    def __init__(self):
+        self.format_str = {}
+        self.format_recreate()
+    def format_recreate(self):
         self.format_str = {
             'camo'         : ''
         }
         
-def textGenerator(self, event):
-        text
+    def textGenerator(self, event):
+        text = GENERATOR[event]
+        return config.i18n[text].format(**self.format_str)
         
-def post_message(self, events):
-    try:
-        _logger.info('message started')
-        g_sessionProvider = BigWorld.player().guiSessionProvider
-        self.format_recreate()
-        for data in events:
-            feedbackEvent = feedback_events.PlayerFeedbackEvent.fromDict(data)
-            eventID = feedbackEvent.getBattleEventType()
-            if eventID in [BATTLE_EVENT_TYPE.VISIBILITY_EVENTS]:
-                vehicleID = feedbackEvent.getTargetID()
-                text
-                inject.message(text)
-    except:
-        _logger.critical('Message Failed')
-print '[LOAD_MOD]: [Camo Indicator by The Illusion]'
+    def post_message(self, events):
+        try:
+            _logger.info('message started')
+            g_sessionProvider = BigWorld.player().guiSessionProvider
+            self.format_recreate()
+            for data in events:
+                feedbackEvent = feedback_events.PlayerFeedbackEvent.fromDict(data)
+                eventID = feedbackEvent.getBattleEventType()
+                    if eventID in [BATTLE_EVENT_TYPE.VISIBILITY_EVENTS]:
+                        ()
+                    text = self.textGenerator(eventID)
+                    inject.message(text)
+        except:
+            _logger.critical('Message Failed')
+print '[LOAD_FINISHED]:  [Camo Indicator by The Illusion v0.01]'
--- mod_constants_camo.py
+++ mod_constants_camo.py
@@ -1,14 +1,45 @@
+
+import math
+
+from constants import ARENA_PERIOD
+from gui.battle_control.battle_constants import CROSSHAIR_VIEW_ID
 from skeletons.gui.app_loader import GuiGlobalSpaceID
 
 class MOD:
-    ID = 'Camo Indicator'
+    ID = '${mod_id}'
     PACKAGE_ID = '${package_id}'
-    NAME = 'Camo Indicator'
+    NAME = '${name}'
     VERSION = '${version}'
 
-#CONFIG_FILES = [
-#    '${resource_dir}/config.json',
-#    '${config_file}'
-#]
+CONFIG_FILES = [
+    '${resource_dir}/default.json',
+    '${resource_dir}/config.json',
+    '${config_file}'
+]
 
 LOG_FILE = '${log_file}'
+
+CROSSHAIR_VIEW_SYMBOL = {
+    CROSSHAIR_VIEW_ID.UNDEFINED:    'UNDEFINED',
+    CROSSHAIR_VIEW_ID.ARCADE:       'ARCADE',
+    CROSSHAIR_VIEW_ID.SNIPER:       'SNIPER',
+    CROSSHAIR_VIEW_ID.STRATEGIC:    'STRATEGIC',
+    CROSSHAIR_VIEW_ID.POSTMORTEM:   'POSTMORTEM'
+}
+
+ARENA_PERIOD_SYMBOL = {
+    ARENA_PERIOD.IDLE:          'IDLE',
+    ARENA_PERIOD.WAITING :      'WAITING',
+    ARENA_PERIOD.PREBATTLE:     'PREBATTLE',
+    ARENA_PERIOD.BATTLE:        'BATTLE',
+    ARENA_PERIOD.AFTERBATTLE:   'AFTERBATTLE'
+}
+
+GUI_GLOBAL_SPACE_SYMBOL = {
+    GuiGlobalSpaceID.UNDEFINED:         'UNDEFINED',
+    GuiGlobalSpaceID.WAITING:           'WAITING',
+    GuiGlobalSpaceID.INTRO_VIDEO:       'INTRO_VIDEO',
+    GuiGlobalSpaceID.LOGIN:             'LOGIN',
+    GuiGlobalSpaceID.LOBBY:             'LOBBY',
+    GuiGlobalSpaceID.BATTLE_LOADING:    'BATTLE_LOADING',
+    GuiGlobalSpaceID.BATTLE:            'BATTLE',
